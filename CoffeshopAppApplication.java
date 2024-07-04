fcaa76e1afe7fd4538277ead58814127612c88e5
Ramansran12 <123183503+Ramansran12@users.noreply.github.com>
2024-07-04 11:57:24 -0400

    Add files via upload

---
+++ CoffeshopAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeshopAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeshopAppApplication.class, args);
+    }
+
+}
---
+++ OrderRepository.java
@@ -0,0 +1,9 @@
+package com.example.coffeeshop;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserId(Long userId);
+}
